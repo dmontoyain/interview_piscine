db95b660e59c064b3a9dd5d7f342ff331d0832a7
Dagoberto Montoya <dmontoya@e1z1r7p24.42.us.org>
2018-04-26 18:30:36 -0700

    binarysearch

---
+++ d02/ex05/findShifted.c
@@ -0,0 +1,49 @@
+#include "header.h"
+#include <stdio.h>
+
+/*
+To return the left-most value found in the shifted arrray
+*/
+int findfirst(int *rocks, int found, int value)
+{
+    int i = 1;
+    int newi = found;
+
+    if (found > 0)
+        while ((found - i) >= 0 && rocks[found - i] == value)
+        {
+            newi = found - i;
+            i++;
+        }
+    return newi;
+}
+
+int binarySearch(int *rocks, int left, int right, int value)
+{
+    int mid;
+    int result;
+
+    printf("left: %d right: %d\n", left, right);
+    if (right >= left)
+    {
+        mid = left + (right - left) / 2;    
+        if (rocks[mid] == value)
+            return mid;
+        if (rocks[left] == value)
+            return left;
+        if (rocks[right] == value)
+            return right;
+        if (rocks[mid] < value)
+        {
+            if ((result = binarySearch(rocks, mid + 1, right, value)) != -1)
+                return result;
+        }
+        return binarySearch(rocks, left, mid - 1, value);
+    }
+    return -1;
+}
+
+int searchShifted(int *rocks, int length, int value)
+{
+    return findfirst(rocks, binarySearch(rocks, 0, length - 1, value), value);
+}
